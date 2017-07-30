840f8e8e82224fc14ae05f13f162ba1f07d11f2e
Samuel Chávez <samuelchvez@gmail.com>
2017-07-30 06:41:03 -0600

    Refactor

--- src/component.js
+++ src/component.js
@@ -1,38 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-// This component transforms any dummy component in a plugin element component.
-class PluginElementComponentWrapper extends Component {
-  componentWillMount() {
-    const { register } = this.props;
-    if (typeof register !== 'undefined') { register(); }
-  }
-
-  render() {
-    const {
-      peId,
-      peConfiguration,
-      peState,
-      DummyComponent,
-      children,
-      ...rest
-    } = this.props;
-
-    if (peState) {
-      return <DummyComponent
-        peId={ peId }
-        peState={ peState }
-        { ...peState }
-        { ...peConfiguration }
-        { ...rest } >
-        { children }
-      </DummyComponent>;
-    }
-
-    return null;
-  }
-}
-
 // This is a second-level higher-order component. It receives a high level
 // description, and returns a higher-order component for a given state tree.
 // The returned higher-order component takes a dummy component (presentational)
@@ -44,32 +12,66 @@ const plugin = ({
   getPluginElementState = (pTree, id) => pTree[id],
   getPluginElementConfiguration = pes => pes.configuration || {},
   getSubstate = state => state[defaultStateKey]
-}) => DummyComponent => connect(
-  (state, { id }) => {
+}) => DummyComponent => {
 
-    // Gets the plugin element state sub-tree
-    const peState = getPluginElementState(getSubstate(state), id);
+  // This component transforms any dummy component in a plugin
+  // element component.
+  class PluginElementComponentWrapper extends Component {
+    componentWillMount() {
+      const { register, peState } = this.props;
+      if (typeof peState === 'undefined') {
+        register();
+      }
+    }
 
-    // If it's registered, pass down these generic props.
-    if (peState) {
+    render() {
+      const {
+        peConfiguration,
+        peState,
+        children,
+        ...rest
+      } = this.props;
+
+      if (peState) {
+        return <DummyComponent
+          peState={ peState }
+          { ...peState }
+          { ...peConfiguration }
+          { ...rest } >
+          { children }
+        </DummyComponent>;
+      }
+
+      return null;
+    }
+  }
+
+  return connect(
+    (state, { id }) => {
+
+      // Gets the plugin element state sub-tree
+      const peState = getPluginElementState(getSubstate(state), id);
+      const peConfiguration = typeof peState === 'undefined' ?
+        undefined:
+        getPluginElementConfiguration(peState);
+
+      // If it's registered, pass down these generic props.
       return {
         peId: id,
         peState,
-        peConfiguration: getPluginElementConfiguration(peState),
-        DummyComponent
+        peConfiguration
+      };
+    },
+    (dispatch, { id, initialState }) => {
+      return {
+        register() {
+          if (typeof initialState !== 'undefined') {
+            dispatch(registerPluginElement(id, initialState));
+          }
+        }
       };
     }
-
-    return {};
-  },
-  (dispatch, { id, initialState }) => ({
-    register() {
-      // If initial state provided, register the plugin element state sub-tree.
-      if (initialState) {
-        dispatch(registerPluginElement(id, initialState));
-      }
-    }
-  })
-)(PluginElementComponentWrapper);
+  )(PluginElementComponentWrapper);
+} 
 
 export default plugin;
